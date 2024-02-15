afb6052c4e8547cd2dee68032f6e46fba152a667
sfiserv <supriya.nirmale@fiserv.com>
2024-02-15 21:28:34 +0000

    pushing DbScript file remotely

---
+++ dbscripts/rn-tenant/dbscripts/production_db_script.js
@@ -0,0 +1,78 @@
+db.tenants.insertOne({
+  title: 'RN Tenant',
+  name: 'RNTenant',
+  tenantHost: 'https://tenant-generic.production-developer-portal.svc',
+  tenantPort: '8443',
+  providerAPIUrl: '/v1/products/RNTenant',
+  apiAuth: {},
+  hasApis: true,
+  productTags: [
+    {
+      category: 'Region', 
+      value: 'Region',
+      tags: ['APAC'],
+    },   
+    {
+      category: 'Integration Type', 
+      value: 'Integration Type',
+      tags: ['restful','sdk'],
+    },  
+    {
+      category: 'Industry', 
+      value: 'Industry',
+      tags: ['Banking , Retail'],
+    },    
+  ],   
+  active: true,
+  betaTag: true,
+  internalTag: true,
+  github: 'rn-tenant',
+  selfServiceFeatures: [
+    {
+      featureName: 'Explore documentation',
+      featureUrl: 'Support/docs/?path=docs/explore-documentation.md',
+      active: true,
+    },
+    {
+      featureName: 'API experimentation with Runbox',
+      featureUrl: 'Support/docs/?path=docs/try-out-the-api-sandbox.md',
+      active: false,
+    },
+    {
+      featureName: 'Generate Credentials',
+      featureUrl: '',
+      active: false,
+    },
+    {
+      featureName: 'Testing & Certification',
+      featureUrl: '',
+      active: false,
+    },
+  ], 
+  gitHubFeatureBranches: [
+    {
+      name: 'active',
+      value: 'main',
+      available: true,
+      hasApis: true,
+      sandboxType: 'mock',
+      mockServerUrl: 'http://tenant-generic-mock-service:8443/sandboxrun',
+    },
+    {
+      name: 'previous',
+      value: 'previous',
+      available: false,
+      hasApis: false,
+      sandboxType: 'mock',
+      mockServerUrl: 'http://tenant-generic-mock-service:8443/sandboxrun',
+    },
+    {
+      name: 'preview',
+      value: 'preview',
+      available: false,
+      hasApis: false,
+      sandboxType: 'mock',
+      mockServerUrl: 'http://tenant-generic-mock-service:8443/sandboxrun',
+    }
+  ]
+})
